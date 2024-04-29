2ec6b08b8063c2c8ad92439085b31bf57834603c
SantiSanGal <santiagon.g.m@gmail.com>
2024-04-29 09:27:31 -0400

    navegacion mal hecha y mpa

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+export const AboutPage = () => {
+  return (
+    <>
+        <h1>About</h1>
+        <div>
+            <img src="/public/images/kitty-baseline.jpg" alt="Yo" />
+        </div>
+        <p>Página de ejemplo para React Router</p>
+        <a href="/">ir a Home</a>
+    </>
+  )
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+  return (
+    <>
+        <h1>Home</h1>
+        <p>Página de ejemplo para React Router</p>
+        <a href="/about">Ir a About</a>   
+    </>
+  )
+}
